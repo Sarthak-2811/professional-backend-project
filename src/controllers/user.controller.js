23cff53170e7d766f5e59e3aba93025531b699fe
Sarthak-2811 <sarthaksavani2811@gmail.com>
2025-11-24 14:12:06 +0530

    fixes logout error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -196,7 +196,7 @@ const logoutUser = asyncHandler(async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {refreshToken: undefined},
+            $unset: {refreshToken: 1},  // this removes field from the document
         },
         {
             new: true
