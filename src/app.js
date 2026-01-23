d2e413897c31c7e9fb3e689ebdc29c84d6177673
Sarthak-2811 <sarthaksavani2811@gmail.com>
2026-01-23 07:00:17 +0530

    feat: Update CORS middleware to allow dynamic origin handling

--- src/app.js
+++ src/app.js
@@ -5,10 +5,29 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 // Middlewares
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(",")
+  : [];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Postman, server calls
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
